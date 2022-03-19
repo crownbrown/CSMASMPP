546d4e3e047f8475731e1477739cb7e7dc815731
Steve Thatcher <sbthatcher@gmail.com>
2022-03-19 12:09:02 -0700

    Update Proj6_ThatChes.asm

--- Proj6_ThatChes.asm
+++ Proj6_ThatChes.asm
@@ -6,12 +6,12 @@ TITLE Program 6 - String Primitives and Macros		    (Proj6_ThatChes.asm)
 ; Course number/section:   CS271 Section 400
 ; Project Number:   6              Due Date:  06/06/2021
 ; Description: This file contains a program which requests 10 integers that fit within a 32-bit signed register. 
-;				All reading and writing is down using Irving Read/Write String ONLY, not use of Read/Write Dec or Int used. 
-;				Reading and writing is down through two macros called throughout the program. 
-;				Integers are read as strings, converted to integers and stored in a 10 element array. 
-;				Integers from arrays are converted back to strings for display. 
-;				Integer strings are displayed, along with sum and average, which is rounded down. 
-;				All loops are tracked and initiated from main, along with all procedure calls. 
+; All reading and writing is done using Irving Read/Write String ONLY, not use of Read/Write Dec or Int used. 
+; Reading and writing is done through two macros called throughout the program. 
+; Integers are read as strings, converted to integers and stored in a 10 element array. 
+; Integers from arrays are converted back to strings for display. 
+; Integer strings are displayed, along with sum and average, which is rounded down. 
+; All loops are tracked and initiated from main, along with all procedure calls. 
 
 INCLUDE Irvine32.inc
 
